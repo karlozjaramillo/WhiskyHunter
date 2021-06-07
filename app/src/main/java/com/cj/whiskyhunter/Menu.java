05b55e36d5a3deeb027e901cffc035fedf764ad4
Karloz <karloz.jaramillo@hotmail.com>
2021-06-06 19:51:49 -0500

    se crea menu

---
+++ app/src/main/java/com/cj/whiskyhunter/Menu.java
@@ -0,0 +1,35 @@
+package com.cj.whiskyhunter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class Menu extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton btnWhiskys;
+    private ImageButton btnDistilleries;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        btnWhiskys = findViewById(R.id.btnWhiskys);
+        btnDistilleries = findViewById(R.id.btnDistilleries);
+        btnWhiskys.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnWhiskys:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
